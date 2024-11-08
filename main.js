23d0f04f70d60b41e3e1b4e5c369368190af735b
Vansh17 <vanshkd17@gmail.com>
2024-11-08 13:13:28 -0500

    changes of svg

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ scene.background = spaceTexture;
 
 // Avatar
 
-const jeffTexture = new THREE.TextureLoader().load('/vansh.JPG');
+const jeffTexture = new THREE.TextureLoader().load('vansh.svg');
 
 const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
 
